27eb72ae54dd0373895aed3a3eae14f56dbc25c0
patrick humphries <patrickhumphries95@gmail.com>
2021-06-11 10:06:29 -0400

    models

--- functions/src/model/IGTruckProfile.ts
+++ functions/src/model/IGTruckProfile.ts
@@ -9,53 +9,87 @@ export default interface IGTruckProfile {
     biography: string;
     external_url: string;
     contact_phone_number: number;
-    latitude: number;
-    longitude: number;
     instagram_location_id: string;
     is_business: boolean;
-    feed: {};
+    feed: Feed;
 }
 
-export default interface Feed {
-    data: [];
+export interface Feed {
+    data: Data[];
 }
 
-export default interface Data {
-    0?:
-    taken_at: number;
-    carousel_media: [
-
-    ];
-    image_versions2" {;
-candidates: [
-    1: {
-        url: string;
-    }
-];
-1 ?:
-    taken_at: number;
-carousel_media: [
-
-];
-image_versions2" {;
-candidates: [
-    1: {
-        url: string;
-    }
-];
-2 ?:
-    taken_at: number;
-carousel_media: [
-
-];
-image_versions2" {;
-candidates: [
-    1: {
-        url: string;
-    }
-]
-
-            }
-];
-};
+export interface Data {
+    post: Post;
 }
+
+export interface Post {
+    taken_at: number;
+    carousel_media?: Carousel[];  
+    location: Location;
+}
+
+export interface Location {
+    name: string;
+    short_name: string;
+    lat: number;
+    lng: number;
+    address: string;
+    city: string;
+}
+
+export interface Carousel {
+    image_versions2: ImageVersion;
+}
+
+export interface ImageVersion {
+    candidates: Candidates; 
+}
+
+export interface Candidates {
+    version: Version;
+}
+
+export interface Version {
+    
+}
+
+
+// export default interface Data {
+//     0?:
+//     taken_at: number;
+//     carousel_media: [
+
+//     ];
+//     image_versions2" {;
+// candidates: [
+//     1: {
+//         url: string;
+//     }
+// ];
+// 1 ?:
+//     taken_at: number;
+// carousel_media: [
+
+// ];
+// image_versions2" {;
+// candidates: [
+//     1: {
+//         url: string;
+//     }
+// ];
+// 2 ?:
+//     taken_at: number;
+// carousel_media: [
+
+// ];
+// image_versions2" {;
+// candidates: [
+//     1: {
+//         url: string;
+//     }
+// ]
+
+//             }
+// ];
+// };
+// }
