f480e78a10db0a69ee10e75986fa27f484a90e1c
Dair Smith <dair.code@gmail.com>
2021-06-11 12:04:23 -0400

    api service

--- frontend/src/service/WtfApiService.ts
+++ frontend/src/service/WtfApiService.ts
@@ -1,7 +1,13 @@
 import IGTruckProfile from "../model/IGTruckProfile";
 import axios from "axios";
 
+const apiKey: string = process.env.API_KEY || "";
+
 const baseUrl = process.env.REACT_APP_API_URL || "";
 if ( !baseUrl ) {
     console.error( "REACT_APP_API_URL environment variable not set." );
+}
+
+export function getTruckData(): Promise<IGTruckProfile[]> {
+    return axios.get( baseUrl ).then( res => res.data );
 }
