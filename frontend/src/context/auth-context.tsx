45fddf74ec108c5dbb1e5b82bd5c144d8945c959
Dair Smith <dair.code@gmail.com>
2021-06-14 13:58:11 -0400

    installed firebase set up

---
+++ frontend/src/context/auth-context.tsx
@@ -0,0 +1,2 @@
+import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import firebase from '../firebaseConfig';
---
+++ frontend/src/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBwu8n9zvqNkx6DeJkTJ2McjV7braCJmp4",
+    authDomain: "wtf-truck.firebaseapp.com",
+    projectId: "wtf-truck",
+    storageBucket: "wtf-truck.appspot.com",
+    messagingSenderId: "740528849850",
+    appId: "1:740528849850:web:24e206d6c8a6f755ce9383"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
