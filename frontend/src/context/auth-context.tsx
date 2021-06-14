8961cf0c638bc6a2a4c15ea8a8443a4e3da1bb1d
Dair Smith <dair.code@gmail.com>
2021-06-14 14:29:30 -0400

    auth context fully set up

--- frontend/src/context/auth-context.tsx
+++ frontend/src/context/auth-context.tsx
@@ -1,2 +1,31 @@
 import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import firebase from '../firebaseConfig';
+
+export interface AuthContextModel {
+    user: firebase.User | null;
+}
+
+const defaultValue: AuthContextModel = {
+    user: null
+};
+
+export const AuthContext = createContext(defaultValue);
+
+export function AuthContextProvider({children}: {children: ReactNode}) {
+    const [ user, setUser ] = useState<firebase.User|null>(null);
+  
+    // Remember useEffect makes sure this code only runs once.
+    useEffect(() => {
+      // The return here passes the unsubscribe function back to useEffect which
+      // will call it when this component is unmounted.
+      return firebase.auth().onAuthStateChanged(newUser => {
+        setUser(newUser);
+      });
+    }, []);
+  
+    return (
+      <AuthContext.Provider value={{ user }}>
+        {children}
+      </AuthContext.Provider>
+    );
+};
