d4f14f02a586227633d0b17ba06f542ee1c35780
Dair Smith <dair.code@gmail.com>
2021-06-15 17:34:44 -0400

    work on map, deployed cloud funcs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,12 @@ function App() {
   return (
     <div className="App" style={{ width: "100vw", height: "100vh" }}>
       <FoodTruckList />
-      <WrappedMap />
+      <WrappedMap 
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`}
+      loadingElement={<div style={{ height: "100%"}} />}
+      containerElement={<div style={{ height: "100%"}} />}
+      mapElement={<div style={{ height: "100%"}} />}
+      />
     </div>
   );
 }
--- functions/src/service/dual-service.ts
+++ functions/src/service/dual-service.ts
@@ -3,12 +3,12 @@ import * as functions from "firebase-functions";
 import IGTruckProfile from "../model/apiModel";
 import axios from "axios";
 
-const key = functions.config().RapidAPI.key;
+// const key = functions.config().RapidAPI.key;
 const host = "https://instagram-bulk-profile-scrapper.p.rapidapi.com/clients/api/ig/bulk_profile";
 
 export function readTruck( handle: string ): Promise<IGTruckProfile> {
   return axios.get( host, {
     params: { ig: handle },
-    headers: { "X-RapidApi-Key": key }
+    headers: { "X-RapidApi-Key": functions.config().RapidAPI.key }
   } ).then( res => res.data[ 0 ] );
 }
