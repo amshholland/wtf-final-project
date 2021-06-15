715b548fe7ce3861d58242cb2aa9e5d4c79c2fe2
patrick humphries <patrickhumphries95@gmail.com>
2021-06-15 09:43:50 -0400

    commit

--- frontend/src/service/WtfApiService.ts
+++ frontend/src/service/WtfApiService.ts
@@ -3,7 +3,7 @@ import { Truck } from "../model/dbModel";
 import trucks from "../components/mongo-clone.json";
 
 const apiKey: string = process.env.API_KEY || "";
-const nodeEnvironment: string = process.env.NODE_ENV || "development";
+const nodeEnvironment: string = process.env.NODE_ENV || "development";  //for offline development
 
 const baseUrl = process.env.REACT_APP_API_URL || "";
 if ( !baseUrl ) {
@@ -12,7 +12,7 @@ if ( !baseUrl ) {
 
 export function getTruckData(): Promise<Truck[]> {
     console.log(nodeEnvironment);
-    if (nodeEnvironment === "development") {
+    if (nodeEnvironment === "development") { //for offline development
         return Promise.resolve(trucks);
     }
     return axios.get( baseUrl ).then( res => res.data );
