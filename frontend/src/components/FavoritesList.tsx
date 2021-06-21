d54e7f027ec3b034acfa5528a0038e56a21fb1ec
Amber Holland <Amber.S.Creech@gmail.com>
2021-06-21 14:35:02 -0400

    renamed dbFavModel

---
+++ frontend/src/components/FavoritesList.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+import { Favorite } from "../model/dbFavModel";
+import { getUserFavorites } from "../service/WtfApiService";
+import { useParams } from "react-router-dom";
+
+interface User {
+    _id: string;
+}
+
+function FavoritesList() {
+    const _id: string = useParams<User>()._id;
+
+    const [ userFavorites, setUserFavorites ] = useState<Favorite[]>( [] );
+    const [ userFavoritesLoaded, setUserFavoritesLoaded ] = useState( false );
+
+    useEffect( () => {
+        // load our initial data here.
+        // loadFavorites( _id );
+    }, [ _id ] );
+
+    // function loadFavorites( _id: string ) {
+    //     if ( user.favorites ) {
+    //         getUserFavorites( _id ).then( fav => {
+    //             setUserFavorites( fav );
+    //             setUserFavoritesLoaded( true );
+    //         } );
+    //     }
+    // }
+}
--- functions/src/routes/wtfRoutes.ts
+++ functions/src/routes/wtfRoutes.ts
@@ -20,5 +20,15 @@ app.get( "/", async ( req, res ) => {
     }
 } );
 
+app.get( "/", async ( req, res ) => {
+    try {
+        const client = await getClient();
+        const results = await client.db().collection<Truck[]>( 'trucks' ).find().toArray();
+        res.json( results ); // send JSON results
+    } catch ( err ) {
+        console.error( "FAIL", err );
+        res.status( 500 ).json( { message: "Internal Server Error" } );
+    }
+} );
 
 export default functions.https.onRequest( app );
